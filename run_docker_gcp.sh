0a1043eed2dd4e2d9f67af72155a2ba06bc9ab50
wojciech.wilusz <wojciech.wilusz@openx.com>
2020-10-24 21:36:35 +0200

    Dropping irrelevant artifacts

--- run_docker_gcp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -ti \
--e "GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/FILE_NAME.json" \
--v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/FILE_NAME.json:ro \
---rm \
---name gcp \
-hegemone \
-bash
