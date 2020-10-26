168792d9683f41c9c984e4f1e5cac2eac5e4b9a8
Chris Pankow <chris.pankow@openx.com>
2020-10-26 10:32:02 -0700

    run.py: add parameter file to request

--- run.py
+++ run.py
@@ -82,6 +82,11 @@ def get_output_notebook_path(template_notebook, gcs_bucket=None):
 
     return out_notebook_fp
 
+def get_flags():
+    import base64
+    with open("etc/config.yml", "r") as fin:
+        prms = fin.read()
+    return base64.b64encode(prms.encode()).decode()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs template notebooks.')
@@ -125,7 +130,6 @@ if __name__ == "__main__":
                                                        gcs_bucket=parameters_dict['google-cloud-platform']['GCS_BUCKET'])
         print("  - destination: {}".format(out_notebook_fp))
         print('Calling Cloud Run hegemone service')
-
         # obtain details about the service
         command = "gcloud run services describe cloud-run-hegemone --format='value(status.url)' --region us-central1 " \
                   "--platform managed"
@@ -136,11 +140,9 @@ if __name__ == "__main__":
         auth_token = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         auth_token = auth_token.stdout.decode("utf-8").replace('\n','')
 
-        # upload a parameters.yaml file
-        # todo: ultimately parameters need to be supplied via a request, not by uploading and overwriting the param file
-        command = 'curl --request POST --header "Authorization: Bearer {TOKEN}" -F file=@parameters.yaml ' \
-                  '{URL}/save_file'.format(TOKEN=auth_token, URL=url)
-        curl = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #todo: mirror the run.py's functionality with curl; send out parameters.yaml
+        run_notebook_command = 'python run.py {IN_NOTEBOOK} -o {OUT_NOTEBOOK}'.format(IN_NOTEBOOK=args.notebook,
+                                                                                      OUT_NOTEBOOK=out_notebook_fp)
 
         # execute the notebook
         run_notebook_command = 'python run.py {IN_NOTEBOOK} -o {OUT_NOTEBOOK} -s {PARAMETERS_SECTION} -k {KERNEL_NAME}'.\
@@ -148,9 +150,10 @@ if __name__ == "__main__":
                    OUT_NOTEBOOK=out_notebook_fp,
                    PARAMETERS_SECTION=args.parameters_section,
                    KERNEL_NAME=args.kernel_name)
+        flags = get_flags()
 
-        command = 'curl --request POST --header "Authorization: Bearer {TOKEN}" --header "Content-Type: text/plain" ' \
-                  '{URL}/exec --data-binary "{COMMAND}"'.format(TOKEN=auth_token, URL=url, COMMAND=run_notebook_command)
+        command = 'curl --request POST --header "Authorization: Bearer {TOKEN}" --header "Content-Type: text/plain" --data parameters="{FLAGS}" ' \
+                  '{URL}/exec --data-binary " {COMMAND}"'.format(TOKEN=auth_token, URL=url, COMMAND=run_notebook_command, FLAGS=flags)
         curl = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Process complete")
     else:
