dfc4506432ddd2f653472e893e44a744967c1fb0
wojciech.wilusz <wojciech.wilusz@openx.com>
2020-10-26 14:34:14 +0100

    A route for uploading parameters.yaml

--- app.py
+++ app.py
@@ -5,24 +5,36 @@ from flask import Flask, request, make_response
 print("Started...")
 app = Flask(__name__)
 
+
 @app.route('/')
 def func1():
     print("Running")
     return "Hello World"
 
+
 @app.route('/exec', methods=['POST'])
 def route_exec():
     command = request.data.decode('utf-8')
     try:
-        completedProcess = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=100, universal_newlines=True)
+        completedProcess = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                          timeout=1000, universal_newlines=True)
         response = make_response(completedProcess.stdout, 200)
         response.mimetype = "text/plain"
         return response
     except subprocess.TimeoutExpired:
-        response = make_response("Timedout", 400)
+        response = make_response("Timedout", 4000)
         response.mimetype = "text/plain"
         return response
     return "/exec"
+
+
+@app.route("/save_file", methods=['POST', 'PUT'])
+def save_file():
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join('./', filename))
+    return filename
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
